d73a35c562650c888c424be9f5efee585f317aee
Eric Wittenberg <ebwittenberg@gmail.com>
2019-02-20 20:17:24 -0500

    Created Paddle class

--- pong.py
+++ pong.py
@@ -2,6 +2,15 @@
 
 import pygame
 
+## Create paddle class
+class Paddle():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed_x = 0
+        self.speed_y = 0
+        # area of the rectangle
+        self.rect = 100
 
 def main():
     #Create screen
@@ -12,12 +21,17 @@ def main():
     screen = pygame.display.set_mode((canvas_width, canvas_height))
     pygame.display.set_caption('Pong Game')
 
+    # create instance of Paddle class
+    paddle = Paddle(250, 250)
+
     done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
         screen.fill((0,0,0))
+        pygame.display.update()
+
 
 main()
     
